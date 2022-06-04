2b5d0c1a25f8aa7306dd945cee52c97c4a41f66f
puzzlerme <70596131+puzzlerme@users.noreply.github.com>
2022-06-03 22:41:28 -0700

    Added admin password and simplified code
    Removed unnecessary variables and added support for admin password

--- htdocs/enterPassword.php
+++ htdocs/enterPassword.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 <body>
 
 <?php 
-    $password = $_GET['password'];
-    $password = hash('sha256', $password);
-    $equal = file_get_contents('password.json') == $password;
-    echo $equal;
+    if (file_get_contents('adminPassword.json') == hash('sha256', $_GET['password'])) {
+        echo "2";
+    } else if (file_get_contents('password.json') == hash('sha256', $_GET['password'])) {
+        echo "1";
+    }
 ?>
 
 </body>
