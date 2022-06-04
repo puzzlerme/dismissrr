1dffb4ab28fc1601bf6c24c2867cffa5c047c505
puzzlerme <70596131+puzzlerme@users.noreply.github.com>
2022-06-03 23:05:27 -0700

    Updated to use htmlspecialchars

--- htdocs/getNames.php
+++ htdocs/getNames.php
@@ -6,7 +6,7 @@
 <body>
 
 <?php
-    if (file_get_contents('adminPassword.json') == hash('sha256', $_GET['password']) or file_get_contents('password.json') == hash('sha256', $_GET['password'])) {
+    if (file_get_contents('adminPassword.json') == hash('sha256', htmlspecialchars($_GET['password'], ENT_QUOTES)) or file_get_contents('password.json') == hash('sha256', htmlspecialchars($_GET['password'], ENT_QUOTES))) {
         echo file_get_contents('studentNames.json');
     }
 ?>
