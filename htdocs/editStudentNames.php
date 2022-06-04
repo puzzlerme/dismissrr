9f222c8f374d3050d0ac1ae9d9f9875406f78d0e
puzzlerme <70596131+puzzlerme@users.noreply.github.com>
2022-06-04 13:09:49 -0700

    Added salting
    Look at the description for index.php's "Added salting" to see reasoning.

--- htdocs/editStudentNames.php
+++ htdocs/editStudentNames.php
@@ -6,8 +6,9 @@
 <body>
 
 <?php
-    if (file_get_contents('adminPassword.json') == hash('sha256', htmlspecialchars($_GET['password'], ENT_QUOTES))) {
-        echo file_put_contents('studentNames.json', htmlspecialchars($_GET['list'], ENT_QUOTES));
+    $salt = "SENtxqzqMrcWIVhShV01.LkLNSJkggNw";
+    if (file_get_contents('adminPassword.json') == hash('sha256', htmlspecialchars($_GET['password'] . $salt, ENT_QUOTES))) {
+        echo file_put_contents('studentNames.json', htmlspecialchars($_GET['list'] . $salt, ENT_QUOTES));
     }
     //header("Location: http://www.dismissrr.rf.gd");
 ?>
