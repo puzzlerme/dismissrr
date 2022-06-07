4c6d3e8de05d63a639ef64a22b2e673a5e200cfd
puzzlerme <70596131+puzzlerme@users.noreply.github.com>
2022-06-07 00:44:37 -0700

    Adding new name to list moved from client to server
    The index.html JS client-side file used to get the list of names and then directly modify it using PHP. The new system has the client only send the new name and the PHP handles all of the adding to the list, which will allow a lot of the client side actions to be removed as well as hopefully removing the chance of multiple people trying to edit it at once and one person overwriting the others.
    *NOT TESTED*

--- htdocs/editStudentNames.php
+++ htdocs/editStudentNames.php
@@ -8,7 +8,11 @@
 <?php
     $salt = "SENtxqzqMrcWIVhShV01.LkLNSJkggNw";
     if (file_get_contents('adminPassword.json') == hash('sha256', htmlspecialchars($_POST['password'], ENT_QUOTES) . $salt)) {
-        echo file_put_contents('studentNames.json', htmlspecialchars($_POST['list'], ENT_QUOTES));
+        $names = file_get_contents('studentNames.json');
+        $names = json_decode($names, true);
+        array_push($names, htmlspecialchars($_POST['name'], ENT_QUOTES));
+        $names = json_encode($names);
+        echo file_put_contents('studentNames.json', $names);
     }
     //header("Location: http://www.dismissrr.rf.gd");
 ?>
